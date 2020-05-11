cadfb74f82d0e09519cbd5a9920ba15de6793140
Manjesh <manjeshmanju123@gmail.com>
2020-05-11 21:29:40 +0400

    First Commit in local rep

---
+++ src/com/array/ex/Array.java
@@ -0,0 +1,15 @@
+package com.array.ex;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] names= {"manjesh","Keerthi"};
+		for(int i=0;i<names.length;i++)
+		{
+			System.out.println("name: "+names[i]);
+		}
+	}
+
+}
